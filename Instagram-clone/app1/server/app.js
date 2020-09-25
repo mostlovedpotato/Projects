bd41dda6f6adec34eb937e35e187b14fece44182
Potato <mostlovedpotato@gmail.com>
2020-09-25 17:02:59 +0530

    day2

--- Instagram-clone/app1/server/app.js
+++ Instagram-clone/app1/server/app.js
@@ -2,8 +2,17 @@ const express = require('express');
 const app = express();
 
 
+const customMiddleware = (req,res,next) => {
+    console.log('middleware encountered');
+    next();
+};
+
+app.use(customMiddleware);
+
+
 app.get('/',(req,res)=>{
+    console.log('home');
     res.send('Hellow World');
 });
 
-app.listen(3000,()=> console.log('Server listening on port no. 3000'));
+app.listen(3000,()=> console.log('Server listening on port no. 4000'));
--- README.md
+++
@@ -1,6 +0,0 @@
-# Projects
-
-
-This is MERN stack two projects alongside 
-1. Ecommerce Site
-2. Instagram clone
